3be71eb943d685940a2da3fdd9b41e9aedd6f71a
Shahad Abdulshakoor <sahmadabdulshakoor@stu.kau.edu.sa>
2023-05-28 14:33:59 +0300

    finished mood tracker function, added input & output files.

--- src/cozyzone/MoodTracker.java
+++ src/cozyzone/MoodTracker.java
@@ -1,18 +1,56 @@
 
 package cozyzone;
-class MoodTracker {
-   private String[] moodOptions[];
+import java.io.PrintWriter;
 
-    public MoodTracker(String[][] moodOptions) {
+ public class MoodTracker {
+   private String moodOptions[] = {"Depressed","Sad","Normal","Pleasant","Happy"};
+   private String[] userMoodArray = new String[2000];// new attribute 
+   private String userMood ;// also new 
+
+    public MoodTracker(String[] moodOptions) {
         this.moodOptions = moodOptions;
     }
 
-    public String[][] getMoodOptions() {
+    public MoodTracker() {
+        this.userMoodArray = userMoodArray ;
+    }
+
+    public String[] getMoodOptions() {
         return moodOptions;
     }
 
-    public void setMoodOptions(String[][] moodOptions) {
+    public void setMoodOptions(String[] moodOptions) {
         this.moodOptions = moodOptions;
     }
+    
+    public void ShowMoodTracker(PrintWriter output){
+        output.println("How are you feeling today ?");
+        output.print("( ");
+        for (int i =0 ; i< moodOptions.length;i++){
+            
+            if(i+1 != moodOptions.length)
+            output.print(moodOptions[i]+" ,");
+              else
+               output.print(moodOptions[i]+" "); 
+        }
+        output.println(")? ");  
+    }  
+    
+    public void SaveMoodInfo(String userMood,int counter){
+        
+        userMoodArray[counter] = userMood;
+        
+    }
+    
+    public void analyzeMoods(PrintWriter output,int counter){
+       output.println("Your previous moods are:");
+       
+        for (int i =0 ; i< counter;i++){
+            output.print(userMoodArray[i]+" ");
+               
+        }
+    }
+    
+    
    
 }
--- src/cozyzone/User.java
+++ src/cozyzone/User.java
@@ -1,34 +1,107 @@
 
 package cozyzone;
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PipedWriter;
+import java.io.PrintWriter;
+
 public class User {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
      
         String userName="";
         int UserId=0;
-      
+        
+        // define file and scanner objects to read input file
+        File inputFile = new File ("userCommands.txt");
+        File outputFile = new File ("outputFile.txt");
+        Scanner scan = new Scanner(inputFile);
+        
+        //define printwriter object to write on the output file
+        PrintWriter output = new PrintWriter(outputFile);
+        
+        //if the file does not exist, the program shall end
+        if (!(inputFile.exists())){
+            System.out.println("The input file does not exists");
+            System.exit(0);
+        }
+        
+        // new attribute 
+        MoodTracker userMood = new MoodTracker();
+        int moodCounter = 0;
+        String mood= null ;
+        
+        //reading from input file
+        String command = null;
+        
+        do{
+            command = scan.next();
+            //write diary command
+            if (command.equalsIgnoreCase("")){
+                
+            }
+            //search diary command
+            else if (command.equalsIgnoreCase("")){
+                
+            }
+            //mood tracker command
+            //to add new mood
+            else if (command.equalsIgnoreCase("addMood")){
+                 command = scan.next();
+                 mood = command;
+                FillMoodTracker(userMood,scan,moodCounter,output,mood);
+                moodCounter++;
+            }
+            //show previous moods
+            else if (command.equalsIgnoreCase("showMood")){
+                
+                AccessMoodTracker(userMood,output,moodCounter);
+                
+            }
+            //answer deep quastions commmand 
+            else if (command.equalsIgnoreCase("")){
+                
+            } 
+            
+        }while (!command.equalsIgnoreCase("QUIT"));
+        
+        
+        //end of program 
+        output.flush();
+        output.close();
+
     }
     
     
-    public Diary RequestNewPage(){
+    public static Diary RequestNewPage(){
         return null;     
     }
-    public Diary Write(){
+    public static Diary Write(){
         return null;
     }
-    public Diary ModifyDiary(){
+    public static Diary ModifyDiary(){
         return null;
     }
+    //this method show report of previous mood trackers filled by the user
+    public static void AccessMoodTracker(MoodTracker userMood, PrintWriter output,int counter){
+      userMood.analyzeMoods(output,counter);
     
-    public void AccessMoodTracker(){
+    }
+    //this method allow user to enter new mood
+    public static void FillMoodTracker(MoodTracker userMood,Scanner scan,int counter,PrintWriter output,String mood){
+       
+        userMood.ShowMoodTracker(output);
+        
+       // String mood = scan.nextLine();
+        output.println("i am feeling "+mood+ "\n");
+        userMood.SaveMoodInfo(mood,counter);
         
     }
-    public void FillMoodTracker(){
-        
-    }
-    public String[] AnswerDeepQuestions(){
+    public static String[] AnswerDeepQuestions(){
         return null;
     }
-    public Diary SearchForDiary(String keyword){
+    public static Diary SearchForDiary(String keyword){
         return null;
     }
 }
